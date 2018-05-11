609d119c03c928b0883f8f89ee46624b613e7b08
Daniel Corn <info@cundd.net>
2018-05-11 20:18:09 +0200

    Add privacy and imprint

---
+++ Imprint/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <?php include "../head.phtml" ?>
+</head>
+<body>
+
+<section id="main">
+    <div class="row">
+        <div class="large-12 columns content">
+            <div class="text">
+                <h1 id="imprint">Imprint</h1>
+                <p>Project by Daniel Corn</p>
+                <p><a href="mailto:support@cundd.net">support@cundd.net</a></p>
+                <p>Made in Vorarlberg</p>
+            </div>
+            <?php include "../footer.phtml" ?>
+        </div>
+    </div>
+</section>
+<?php include "../body_end.phtml" ?>
+</body>
+</html>
---
+++ Privacy/index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html class="no-js" lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <?php include "../head.phtml" ?>
+</head>
+<body>
+<section id="main">
+    <div class="row">
+        <div class="large-12 columns content">
+            <div class="text">
+                <h1 id="privacy">Privacy</h1>
+                <p>This site uses Matomo to analyze traffic and help us to improve your user experience.</p>
+                <p>We process your IP address and cookies are stored on your browser for 13 months.
+                    This data is only processed by us and our web hosting platform. Please read our
+                    Privacy Policy to learn more.</p>
+                <h2 id="1-----purpose-of-the-processing">1 – Purpose of the processing</h2>
+                <p>Matomo is used to analyse the behaviour of the website visitors to identify potential
+                    pitfalls; not found pages, search engine indexing issues, which contents are the most
+                    appreciated… Once the data is processed (number of visitors reaching a not found pages,
+                    viewing only one page…), Matomo is generating reports for website owners to take action,
+                    for example changing the layout of the pages, publishing some fresh content… etc.</p>
+                <p>Matomo is processing the following personal data:</p>
+                <ul>
+                    <li>Cookies</li>
+                    <li>IP address</li>
+                    <li>Custom Dimensions</li>
+                    <li>Approximate location of the user</li>
+                </ul>
+                <p>And also:</p>
+                <ul>
+                    <li>Date and time</li>
+                    <li>Title of the page being viewed</li>
+                    <li>URL of the page being viewed</li>
+                    <li>URL of the page that was viewed prior to the current page</li>
+                    <li>Screen resolution</li>
+                    <li>Time in local timezone</li>
+                    <li>Files that were clicked and downloaded</li>
+                    <li>Link clicks to an outside domain</li>
+                    <li>Pages generation time</li>
+                    <li>Country, region, city</li>
+                    <li>Main Language of the browser</li>
+                    <li>User Agent of the browser</li>
+                </ul>
+                <p>The processing of personal data with Matomo is based on legitimate interests.</p>
+                <h2 id="2-----the-legitimate-interests">2 – The legitimate interests</h2>
+                <p>Processing your personal data such as cookies is helping us identify what is working
+                    and what is not on our website. For example, it helps us identify if the way we are
+                    communicating is engaging or not and how we can organize the structure of the website
+                    better. Our team is benefiting from the processing of your personal data, and they are
+                    directly acting on the website. By processing your personal data, you can profit from
+                    a website which is getting better and better.</p>
+                <p>Without the data, we would not be able to provide you the service we are currently
+                    offering to you. Your data will be used only to improve the user experience on our
+                    website and help you find the information you are looking for.</p>
+                <h2 id="3-----recipient-of-the-personal-data">3 – Recipient of the personal data</h2>
+                <p>The personal data received through Matomo are only sent to us.</p>
+                <h2 id="4-----details-of-transfers-to-third-country-and-safeguards">4 – Details of transfers to third
+                    country and safeguards</h2>
+                <p>Matomo data is hosted in Germany.</p>
+                <h2 id="5-----retention-period-or-criteria-used-to-determine-the-retention-period">5 – Retention period
+                    or
+                    criteria used to determine the retention period</h2>
+                <p>We are keeping the personal data captured within Matomo for a period of 150 days.</p>
+                <h2 id="6-----the-existence-of-each-of-the-data-subject---s-rights">6 – The existence of each of the
+                    data
+                    subject’s rights</h2>
+                <p>As Matomo is processing personal data on legitimate interests, you can exercise the
+                    following rights:</p>
+                <ul>
+                    <li>Right of access: you can ask us at any time to access your personal data.</li>
+                    <li>Right to erasure: you can ask us at any time to delete all the personal data we are
+                        processing about you.
+                    </li>
+                    <li>Right to object: you can object to the tracking of your personal data by using the
+                        following opt-out feature:
+                    </li>
+                </ul>
+
+                <iframe style="border: 0; height: 200px; width: 600px;"
+                        src="https://piwik.cundd.net/index.php?module=CoreAdminHome&amp;action=optOut&amp;language=de&amp;backgroundColor=&amp;fontColor=f45&amp;fontSize=14px&amp;fontFamily=%22Inconsolata%22%2C%20monospace"></iframe>
+                <p>If you wish us to not process any personal data with Matomo, you can opt-out from it at
+                    any time. There will be no consequences at all regarding the use of our website.</p>
+                <p>Matomo is not doing any profiling.</p>
+            </div>
+
+            <?php include "../footer.phtml" ?>
+        </div>
+    </div>
+
+</section>
+<?php include "../body_end.phtml" ?>
+</body>
+</html>
